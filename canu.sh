e5d0594bb056715400b1c266656f207701a256ee
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-05-20 10:15:19 +0200

    Create canu.sh

---
+++ canu.sh
@@ -0,0 +1,7 @@
+/usit/abel/u1/mortema/software/canu/Linux-amd64/bin/canu \
+ -p asm -d yeast \
+ genomeSize=12.1m \
+ corMhapSensitivity=high corMinCoverage=2 errorRate=0.035 minOverlapLength=499 corMaxEvidenceErate=0.3 \
+-java="/usr/bin/java" \
+-pacbio-raw yeast.20x.fastq.gz \
+-gridOptions='--account=cees --time=24:00:00'
