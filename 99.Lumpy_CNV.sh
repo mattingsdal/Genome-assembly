c2c66a5140e32b3b1804cfe9d0fd6f994a5c7d6c
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-06-09 12:56:56 +0200

    Update 99.Lumpy_CNV.sh

--- 99.Lumpy_CNV.sh
+++ 99.Lumpy_CNV.sh
@@ -1,18 +1,13 @@
 
       for D in *bam
-
         do
-
-
         # Extract the discordant paired-end alignments.
         samtools view -b -F 1294 $D.bam > $D.discordants.unsorted.bam
-
         # Extract the split-read alignments
         samtools view -h $D \
         | scripts/extractSplitReads_BwaMem -i stdin \
         | samtools view -Sb - \
         > $D.splitters.unsorted.bam
-
         # Sort both alignments
         samtools sort sample.discordants.unsorted.bam sample.discordants
         samtools sort sample.splitters.unsorted.bam sample.splitters
