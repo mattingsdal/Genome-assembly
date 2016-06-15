109ae679897fae19fc92207c8787e973fa6bf518
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-06-15 14:55:48 +0200

    Update 55.statistics_assembly.sh

--- 55.statistics_assembly.sh
+++ 55.statistics_assembly.sh
@@ -3,7 +3,7 @@
 fastafile=symphodus_melops.fasta
 
 # RepeatMasker quick search using 4 CPU's at the lineage "teleostei"
-RepeatMasker -species=teleostei $fastafile -q -pa 4
+~/software/maker/exe/RepeatMasker/RepeatMasker -species=teleostei $fastafile -q -pa 4
 
 #contig length, gc content etc
 python ~/software/quast-4.0/quast.py $fastafile -o quast_out -t 4
