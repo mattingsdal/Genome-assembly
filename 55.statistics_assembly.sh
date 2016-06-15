0cdfcf2b4bf77d2a250590f49b2429ea8c3682f8
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-06-15 14:30:58 +0200

    Update 55.statistics_assembly.sh

--- 55.statistics_assembly.sh
+++ 55.statistics_assembly.sh
@@ -1,1 +1,11 @@
 # create descripteve statistics for a fasta assembly
+
+fastafile=symphodus_melops.fasta>la
+
+# RepeatMasker
+RepeatMasker -species=teleostei $fastafile -q -pa 4
+
+#contig length, gc content etc
+infoseq -auto -only -accession -length -pgc $fastafile
+
+
