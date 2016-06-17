a23fc343294bcfd62996a448be91ebd07e0db34c
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-06-17 09:37:45 +0200

    Update 55.statistics_assembly.sh

--- 55.statistics_assembly.sh
+++ 55.statistics_assembly.sh
@@ -9,5 +9,14 @@ fastafile=symphodus_melops.fasta
 python ~/software/quast-4.0/quast.py $fastafile -o quast_out -t 4
 
 # CpG islands (parameters are from TJ alogithmn from PubMed ID:18483567) use repeatmasked seqience
-cpgplot symphodus_melops.fasta -window 200 -minlen 500 -minoe 0.65 -minpc 55 -outfile $fastafile.gc -outfeat $fastafile.gc.gff -noplot
+# cpgplot symphodus_melops.fasta -window 200 -minlen 500 -minoe 0.65 -minpc 55 -outfile $fastafile.gc -outfeat $fastafile.gc.gff -noplot
 
+# CpG island prediction TJ with parameters like PubMed ID:18483567
+perl CpGi130.pl $fastafile GCC=55 OE=65 LENGTH=500
+
+#RepeatModeler on masked genome sequence
+~/software/RepeatModeler/BuildDatabase -engine ncbi -name "ballan" ../ballan.fasta.masked
+~/software/RepeatModeler/RepeatModeler -engine ncbi -pa 2 -database ballan >& RepeatModeler.out &
+
+~/software/RepeatModeler/BuildDatabase -engine ncbi -name "corkwing" ../symphodus_melops.fasta.masked
+~/software/RepeatModeler/RepeatModeler -engine ncbi -pa 2 -database corkwing >& RepeatModeler.out &
