d70abf5bb7939dc92da4db910062972cb5bac3f2
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-11-03 11:35:29 +0100

    Update 01.canu.sh

--- 01.canu.sh
+++ 01.canu.sh
@@ -1,17 +1,10 @@
-# example x20 yeast data
+ssh abel
 
-/usit/abel/u1/mortema/software/canu/Linux-amd64/bin/canu \
- -p asm -d yeast \
- genomeSize=12.1m \
- corMhapSensitivity=high corMinCoverage=2 errorRate=0.035 minOverlapLength=499 corMaxEvidenceErate=0.3 \
--java="/usr/bin/java" \
--pacbio-raw yeast.20x.fastq.gz \
--gridOptions='--account=cees --time=24:00:00'
+ssh cod7
 
-# testing on 1 fastq file, actual data
-~/software/canu/Linux-amd64/bin/canu -p corkwing\
--d /usit/abel/u1/mortema/pacbio \
-genomeSize=600000000 \
--pacbio-raw *fastq.gz \
-gnuplot=/usit/abel/u1/mortema/bin/bin/gnuplot \
--gridOptions='--account=cees --time=24:00:00'
+cd pacbio
+
+nohup ~/software/canu/Linux-amd64/bin/canu -p corkwing -d /usit/abel/u1/mortema/pacbio/run1 genomeSize=600000000 -pacbio-raw *fastq.gz gnuplot=/usit/abel/u1/mortema/bin/bin/gnuplot &
+
+
+
