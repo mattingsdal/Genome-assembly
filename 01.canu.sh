536b6f6c21e6c6f164094ee1975b9762b124002a
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-11-14 00:33:10 +0100

    Update 01.canu.sh

--- 01.canu.sh
+++ 01.canu.sh
@@ -4,5 +4,9 @@ ssh cod7
 
 cd pacbio
 
-nice -n 8 nohup ~/software/canu/Linux-amd64/bin/canu -p corkwing -d /usit/abel/u1/mortema/pacbio/run1 maxMemory=384 maxThreads=48 ovsConcurrency=8 ovbConcurrency=8 -useGrid=false genomeSize=600000000 -pacbio-raw *fastq gnuplot=/usit/abel/u1/mortema/bin/bin/gnuplot &
+# run2
+nice -n 5 nohup ~/software/canu/Linux-amd64/bin/canu -p corkwing -d /usit/abel/u1/mortema/pacbio/run2 maxMemory=384 maxThreads=32 ovsConcurrency=8 ovbConcurrency=8 -useGrid=false genomeSize=600000000 -pacbio-raw *fastq gnuplot=/usit/abel/u1/mortema/bin/bin/gnuplot &
+
+# run3
+nice -n 5 nohup ~/software/canu/Linux-amd64/bin/canu -p corkwing -d /usit/abel/u1/mortema/pacbio/run3 corMhapSensitivity=high corMinCoverage=0 maxMemory=384 maxThreads=32 ovsConcurrency=8 ovbConcurrency=8 -useGrid=false genomeSize=600000000 -pacbio-raw *fastq gnuplot=/usit/abel/u1/mortema/bin/bin/gnuplot &
 
