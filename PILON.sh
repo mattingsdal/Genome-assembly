f34d812ccc96a99e3fb13848bde47b91a31651f3
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2017-02-15 13:28:15 +0100

    Create PILON.sh

---
+++ PILON.sh
@@ -0,0 +1,4 @@
+module laod java
+java -Xmx200G -jar /usit/abel/u1/mortema/software/pilon-1.21/pilon-1.21.jar --genome /usit/abel/u1/mortema/genome/corkwing.contigs.fasta --frags \
+"bamfiles"
+--tracks --changes --vcf --diploid --output Pilon_1 1> pilonRun.out 2> pilonRun.err
