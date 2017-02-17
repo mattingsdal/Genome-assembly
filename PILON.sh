8291212c7c585584e9a7186616fc4365d78646e4
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2017-02-17 09:45:11 +0100

    Update PILON.sh

--- PILON.sh
+++ PILON.sh
@@ -1,4 +1,2 @@
 module laod java
-java -Xmx200G -jar /usit/abel/u1/mortema/software/pilon-1.21/pilon-1.21.jar --genome /usit/abel/u1/mortema/genome/corkwing.contigs.fasta --frags \
-"bamfiles"
---tracks --changes --vcf --diploid --output Pilon_1 1> pilonRun.out 2> pilonRun.err
+java -Xmx200G -jar /usit/abel/u1/mortema/software/pilon-1.21/pilon-1.21.jar --genome /usit/abel/u1/mortema/genome/corkwing.contigs.fasta --frags HG5KLBCXY_Smelops_GATCAG_L001.bam --tracks --changes --vcf --diploid --output Pilon_1 1> pilonRun.out 2> pilonRun.err
